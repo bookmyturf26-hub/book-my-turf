a14392d48a23b55fd163ddb67fea3d6ec14cb28f
unknown <sanjanaoza02@gmail.com>
2026-01-24 16:48:38 +0530

    Added backend source code and files

---
+++ back-end/src/main/java/com/bookmyturf/dto/UserRegisterDTO.java
@@ -0,0 +1,44 @@
+
+	package com.bookmyturf.dto;
+
+	public class UserRegisterDTO {
+
+	    private String firstName;
+	    private String lastName;
+	    private String emailAddress;
+	    private String password;
+	    private String permanentAddress;
+	    private String cityName;
+	    private String contactPhoneNo;
+	    private String accountStatus;
+	    private String userType;
+
+	    public String getFirstName() { return firstName; }
+	    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+	    public String getLastName() { return lastName; }
+	    public void setLastName(String lastName) { this.lastName = lastName; }
+
+	    public String getEmailAddress() { return emailAddress; }
+	    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+
+	    public String getPassword() { return password; }
+	    public void setPassword(String password) { this.password = password; }
+
+	    public String getPermanentAddress() { return permanentAddress; }
+	    public void setPermanentAddress(String permanentAddress) { this.permanentAddress = permanentAddress; }
+
+	    public String getCityName() { return cityName; }
+	    public void setCityName(String cityName) { this.cityName = cityName; }
+
+	    public String getContactPhoneNo() { return contactPhoneNo; }
+	    public void setContactPhoneNo(String contactPhoneNo) { this.contactPhoneNo = contactPhoneNo; }
+
+	    public String getAccountStatus() { return accountStatus; }
+	    public void setAccountStatus(String accountStatus) { this.accountStatus = accountStatus; }
+
+	    public String getUserType() { return userType; }
+	    public void setUserType(String userType) { this.userType = userType; }
+	}
+
+

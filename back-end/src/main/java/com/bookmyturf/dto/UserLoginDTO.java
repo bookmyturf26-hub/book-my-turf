1549413fa7f4ca6d9ab19b2a1566103c89dac462
unknown <sanjanaoza02@gmail.com>
2026-01-26 02:16:23 +0530

    Added unique constraint for email and phone in User entity also updated with required dto

---
+++ back-end/src/main/java/com/bookmyturf/dto/UserLoginDTO.java
@@ -0,0 +1,17 @@
+package com.bookmyturf.dto;
+
+public class UserLoginDTO {
+
+   
+    private String emailAddress;
+
+  
+    private String password;
+
+    // Getters and Setters
+    public String getEmailAddress() { return emailAddress; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
